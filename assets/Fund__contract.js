6ed9e35c0110ce87cf658cacfc453a721118d575
duckmint864 <nddminh2021@gmail.com>
2023-07-13 00:34:02 +0700

    .

---
+++ assets/Fund__contract.js
@@ -0,0 +1,137 @@
+export const ABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "priceFeedAddress",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "Fund__BelowMinimum",
+        "type": "error"
+    },
+    {
+        "inputs": [],
+        "name": "Fund__NotOwner",
+        "type": "error"
+    },
+    {
+        "stateMutability": "payable",
+        "type": "fallback"
+    },
+    {
+        "inputs": [],
+        "name": "MINIMUM_USD",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "fund",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "funder",
+                "type": "address"
+            }
+        ],
+        "name": "getAddressToAmount",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "index",
+                "type": "uint256"
+            }
+        ],
+        "name": "getFunder",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getFundersCount",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getOwner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getPriceFeed",
+        "outputs": [
+            {
+                "internalType": "contract AggregatorV3Interface",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "withdraw",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "stateMutability": "payable",
+        "type": "receive"
+    }
+]
+
+export const CONTRACT_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // Dependencies
 import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.4.0/ethers.min.js"
+import { ABI, CONTRACT_ADDRESS } from "./assets/Fund__contract.js";
 
 
-// before all
-let metamaskInstalled = (typeof window.ethereum !== "undefined") ? true : false;;
-let signerAddress;
+/* before all */
+let METAMASK_INSTALLED = (typeof window.ethereum !== "undefined") ? true : false;;
+let PROVIDER;
+let SIGNER;
+let FUND;
 
-// Get button objects in from DOM
+
+// Get button objects from DOM
 let showAccount = document.getElementById("showAccount");
 let connectButton = document.getElementById("connectButton");
 let fundButton = document.getElementById("fundButton");
@@ -16,63 +20,151 @@ let getPriceFeedButton = document.getElementById("getPriceFeedButton");
 let getFunderToAmountButton = document.getElementById("getFunderToAmountButton");
 
 
+// Get input objects from DOM
+let ethAmountInput = document.getElementById("ethAmountInput");
+let funderIndexInput = document.getElementById("funderIndexInput");
+let funderAddressInput = document.getElementById("funderAddressInput");
+
+
+// Get response objects from DOM 
+// (connect button don't have a dedicated response object because the response is meant to overwrite the butotn's content)
+let fundButtonResponse = document.getElementById("fundButtonResponse");
+let withdrawButtonResponse = document.getElementById("withdrawButtonResponse");
+let getFunderButtonResponse = document.getElementById("getFunderButtonResponse");
+let getPriceFeedButtonResponse = document.getElementById("getPriceFeedButtonResponse");
+let getFunderToAmountButtonResponse = document.getElementById("getFunderToAmountButtonResponse");
+
+
 // Handle connect button
 async function connect() { // Connect D-app to Ethereum account via metamask
-    if (!metamaskInstalled) {
-        window.alert("Please install metamask 🦊");
-        showAccount.innerHTML = "Please install metamask 🦊";
+    if (!METAMASK_INSTALLED) {
+        window.alert("Please install a wallet first 🦊");
+        showAccount.innerHTML = "Please install a wallet first 🦊";
     } else {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-            .catch((err) => {
-                if (err.code === 4001) {
-                    // EIP-1193 userRejectedRequest error
-                    // If this happens, the user rejected the connection request.
-                    console.log("LOL💀☠️");
-                } else {
-                    console.error(err);
-                }
-            });
-        signerAddress = accounts[0];
-        showAccount.innerHTML = signerAddress;
-        document.getElementById("connectButton").innerHTML = "Connected";
+        try {
+            PROVIDER = new ethers.BrowserProvider(window.ethereum);
+            SIGNER = await PROVIDER.getSigner();
+            showAccount.innerHTML = await SIGNER.getAddress();
+            FUND = new ethers.Contract(CONTRACT_ADDRESS, ABI, SIGNER); // create a contract instance asigned to the SIGNER
+
+            // Response
+            connectButton.innerHTML = "Connected";
+        } catch (err) {
+            window.alert("--> connect() failed^ Reason: view in console");
+            console.log("--> connect() failed^ Reason:\n", err);
+            console.log("-----------------------------------------------------------------------------------------------");
+        }
     }
 }
 
 
 // Handle fund button
-async function fund(amount) {
+async function fund(ethAmount) { // amount unit: ETH
+    try {
+        await FUND.fund({
+            value : ethers.parseEther(ethAmount)
+        });
 
+        // Response
+        fundButtonResponse.innerHTML = ("You have funded");
+    } catch(err) {
+        window.alert("--> fund() failed^ Reason: view in console")
+        console.log("--> fund() failed^ Reason:\n", err);
+        console.log("-----------------------------------------------------------------------------------------------");
+    }
 }
 
 
 // Handle withdraw button
 async function withdraw() {
+    try {
+        await FUND.withdraw();
 
+        // Response
+        withdrawButtonResponse.innerHTML = ("You have withdrawn");
+    } catch(err) {
+        window.alert("--> withdraw() failed^ Reason: view in console")
+        console.log("--> withdraw() failed^ Reason:\n", err);
+        console.log("-----------------------------------------------------------------------------------------------");
+    }
 } 
 
 
 // Handle get funder button
-async function getFunder() {
+async function getFunder(funderIndex) {
+    let funderAddress = null;
+    try {
+        funderAddress = await FUND.getFunder(funderIndex);
 
+        // Response
+        getFunderButtonResponse.innerHTML = ("Address of funder: " +  funderAddress);
+    } catch(err) {
+        window.alert("--> getFunder() failed^ Reason: view in console")
+        console.log("--> getFunder() failed^ Reason:\n", err);
+        console.log("-----------------------------------------------------------------------------------------------");
+    }
+    return funderAddress;
 }
 
 
 // Handle get price feed button
 async function getPriceFeed() {
+    let priceFeedAddress = null;
+    try {
+        priceFeedAddress = await FUND.getPriceFeed();
 
+        // Response
+        getPriceFeedButtonResponse.innerHTML = ("Price feed address: " + priceFeedAddress);
+    } catch(err) {
+        window.alert("--> getPriceFeed() failed^ Reason: view in console")
+        console.log("--> getPriceFeed() failed^ Reason:\n", err);
+        console.log("-----------------------------------------------------------------------------------------------");
+    }
+    return priceFeedAddress;
 }
 
 
 // Handle get funder to amount button
-async function getFunderToAmount() {
+async function getFunderToAmount(funderAddress) {
+    let amount = null;
+    try {
+        amount = await FUND.getFunderToAmount(funderAddress);
 
+        // Response
+        getFunderToAmountButtonResponse.innerHTML = ("Amount: " + amount);
+    } catch(err) {
+        window.alert("--> getFunderToAmount() failed^ Reason: view in console")
+        console.log("--> getFunderToAmount() failed^ Reason:\n", err);
+        console.log("-----------------------------------------------------------------------------------------------");
+    }
+    return amount;
 }
 
 
-// Attach buttons to their corresponding function
-connectButton.onclick = connect;
-fundButton.onclick = fund;
-withdrawButton.onclick = withdraw;
-getFunderButton.onclick = getFunder;
-getPriceFeedButton.onclick = getPriceFeed;
-getFunderToAmountButton.onclick = getFunderToAmount;
+// Make DOM objects listen for event 'click'
+connectButton.addEventListener("click", () => {
+    connect();
+});
+
+fundButton.addEventListener("click", () => {    
+    const ethAmount = ethAmountInput.value;
+    fund(ethAmount);
+});
+
+withdrawButton.addEventListener("click", () => {
+    withdraw();
+});
+
+getFunderButton.addEventListener("click", () => {
+    const funderIndex = funderIndexInput.value; 
+    getFunder(funderIndex);
+});
+
+getPriceFeedButton.addEventListener("click", () => {
+    getPriceFeed();
+});
+
+getFunderToAmountButton.addEventListener("click", () => {
+    const funderAddress = funderAddressInput.value;
+    getFunderToAmount(funderAddress);
+});
