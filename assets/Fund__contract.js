18dad7ba145c5daef0ce414b0b28de7026f0e54c
duckmint864 <nddminh2021@gmail.com>
2023-07-13 00:38:02 +0700

    .

--- assets/Fund__contract.js
+++ assets/Fund__contract.js
@@ -134,4 +134,5 @@ export const ABI = [
     }
 ]
 
+// This address persists accross multiple startup of local blockchain, so no need to worry about it.
 export const CONTRACT_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
